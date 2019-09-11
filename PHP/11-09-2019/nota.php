cfebc970dbbc077c8b2a4c396d38668b1c09e4d0
andreyrehv <andreycaldas@gmail.com>
2019-09-11 06:25:31 -0300

    objeto

--- PHP/11-09-2019/nota.php
+++ PHP/11-09-2019/nota.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $n1 = $_POST["n1"];
 $n2 = $_POST["n2"];
 $n3 = $_POST["n3"];
@@ -12,5 +12,35 @@ if($media >= 7){
 }else{
 	echo "Aluno Reprovado!";
 }	
+*/
+$n1 = $_POST["n1"];
+$n2 = $_POST["n2"];
+$n3 = $_POST["n3"];
+$n4 = $_POST["n4"];
+
+class mediaAlunos{
+	
+	public $media;
+	
+	public function calcular($n1, $n2, $n3, $n4){
+		$media = (($n1+$n2+$n3+$n4)/4);
+			
+		echo "A media do Aluno é: ". $media . "<br><br>" ;
+		
+		if($media >= 7){
+		echo "Aluno Aprovado!";
+	}else{
+		echo "Aluno Reprovado!";
+	}	
+	
+	}
+}
+
+$obj = new mediaAlunos();
+$obj -> calcular($n1, $n2, $n3, $n4);	
+	
+	
+
+
 
 ?>
